c9d3bba585367ce7d538e6cc3e3b43052a980ed6
JordanKra <jkrasan@umich.edu>
2022-03-14 22:53:11 -0400

    Fixed buggy output

--- Project #3/oneLaneBridge.c
+++ Project #3/oneLaneBridge.c
@@ -10,55 +10,68 @@ pthread_mutex_t cross_mutex;
 pthread_cond_t cross_cv;
 //Keep track of the direction of the car currently on the bridge
 int current_dir = -1;
-bool bridge_occupied = false;
 int num_on_bridge = 0;
+//Setup a struct to store the data of each thread
 struct args{
     int dir;
     int num;
 };
 
-
+bool safeToCross(int dir){
+    //determine based on car direction and how many cars are on the bridge if its safe to cross
+    if(num_on_bridge == 0){
+        return true;
+    }
+    else if(current_dir == dir && num_on_bridge < 3){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 void ArriveBridge(int dir, int num){
     printf("Car %d direction %d arrives at the bridge\n",num, dir);
     pthread_mutex_lock(&cross_mutex);
-    while(bridge_occupied){
-        if(current_dir == dir && num_on_bridge < 3){
-            //We can cross the bridge as well
-            break;
-        }
+
+    while(!safeToCross(dir)){
         pthread_cond_wait(&cross_cv, &cross_mutex);
     }
-    //set current_dir to be current threads dir
+    //set current_dir to be current threads dir and increase the car count by 1
     current_dir = dir;
-    bridge_occupied = true;
     num_on_bridge++;
+
     pthread_mutex_unlock(&cross_mutex);
     sched_yield();
 }
 
 void CrossBridge(int num){
-    pthread_mutex_lock(&cross_mutex);
     printf("Car %d is crossing the bridge. Current dir %d #cars: %d\n",num, current_dir, num_on_bridge);
-    pthread_mutex_unlock(&cross_mutex);
 }
 
 void ExitBridge(int dir, int num){
+
     pthread_mutex_lock(&cross_mutex);
+
     //Weve reached the other side of the bridge so we can set bridge_occupied to false
     //and signal for the next thread
     printf("Car number %d has exited the bridge with dir: %d\n", num, dir);
-    bridge_occupied = false;
     num_on_bridge--;
     pthread_cond_broadcast(&cross_cv);
+
     pthread_mutex_unlock(&cross_mutex);
     sched_yield();
 }
 
 void *OneVehicle(void *thread_args){
-    struct args *arguments = (struct args*)thread_args;
-    ArriveBridge((*arguments).dir, (*arguments).num);
-    CrossBridge((*arguments).num);
-    ExitBridge((*arguments).dir, (*arguments).num);
+    struct args arguments = *((struct args*)thread_args);
+
+    ArriveBridge(arguments.dir, arguments.num);
+
+    CrossBridge(arguments.num);
+
+    ExitBridge(arguments.dir, arguments.num);
+    
+    //Free allocated data once car is done crossing the bridge
     free(thread_args);
     return 0;
 }
@@ -66,8 +79,12 @@ void *OneVehicle(void *thread_args){
 int main(int argc, char *argv[]){
     int dir = 0;
     int num_cars = 0;
+    pthread_attr_t attr;
     pthread_mutex_init(&cross_mutex, NULL);
     pthread_cond_init(&cross_cv, NULL);
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
     if(argc < 2){
         printf("Usage: oneLaneBridge <Number of Cars>\n");
         printf(" e.g.: ./oneLaneBridge 50\n");
@@ -83,12 +100,13 @@ int main(int argc, char *argv[]){
         (*arguments).dir = rand() % 2;
         (*arguments).num = i;
 
-        pthread_create(&threads[i],NULL, &OneVehicle, (void*)arguments);
+        pthread_create(&threads[i], &attr, &OneVehicle, (void*)arguments);
     }
     for(int i = 0; i < num_cars; i++){
         pthread_join(threads[i], NULL);
     }
 
+    pthread_attr_destroy(&attr);
     pthread_mutex_destroy(&cross_mutex);
     pthread_cond_destroy(&cross_cv);
     pthread_exit (NULL);
--- Project #3/professorStudent.c
+++ Project #3/professorStudent.c
@@ -18,14 +18,15 @@ void AnswerStart(){
 }
 
 void AnswerDone(){
-    sem_post(&answer_done);
-    sem_post(&question_in_progress);
     printf("The professor has finished answering a question\n");
+    sem_post(&answer_done);
+
     sched_yield();
 }
 
-void QuestionStart(){
+void QuestionStart(int student_number){
     //Wait for the professor to be ready
+    printf("Student number %d is ready to ask a question\n", student_number);
     sem_wait(&professor_ready);
     //Wait until the student can ask a question
     sem_wait(&question_in_progress);
@@ -37,12 +38,12 @@ void QuestionDone(int student_number){
     sem_post(&ready_for_answer);
     sem_wait(&answer_done);
     printf("Student number: %d has recieved an answer and has left\n", student_number);
-
+    sem_post(&question_in_progress);
     sched_yield();
 }
 
 void *TheProfessor(void *num_students){
-    int i = *(int*)num_students;
+    int i = *((int*)(num_students));
     while(i > 0){
         AnswerStart();
         printf("The professor is answering a question\n");
@@ -54,11 +55,11 @@ void *TheProfessor(void *num_students){
 }
 
 void *OneStudent(void *stuNum){
-    int student_number = *(int*)stuNum;
-    printf("Student number %d is ready to ask a question\n", student_number);
-    QuestionStart();
+    int student_number = *((int*)(&stuNum));
+    QuestionStart(student_number);
     printf("Student Number %d is asking a question\n", student_number);
     QuestionDone(student_number);
+    pthread_exit(0);
     return 0;
 }
 
@@ -68,6 +69,7 @@ int main(int argc, char *argv[]){
     sem_init(&question_in_progress, SHARED, 1);
     sem_init(&ready_for_answer, SHARED, 0);
     sem_init(&answer_done, SHARED, 0);
+    pthread_attr_t attr;
 
     if(argc < 2){
         printf("Usage: ./professorStudent <Number of students>\n");
@@ -75,19 +77,22 @@ int main(int argc, char *argv[]){
         exit(0);
     }
     
+    pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
     numStudent = atoi(argv[1]);
     pthread_t threads[numStudent];
     pthread_t professor;
-
-    pthread_create(&professor, NULL,&TheProfessor, (void*)&numStudent);
+    pthread_create(&professor, &attr,&TheProfessor, (void*)&numStudent);
     for(int i = 0; i < numStudent; i++){
-        pthread_create(&threads[i],NULL, &OneStudent, (void*)&i);
+        pthread_create(&threads[i], &attr, OneStudent, (void*)i);
     }
     
     for(int i = 0; i < numStudent; i++){
         pthread_join(threads[i], NULL);
     }
     pthread_join(professor,NULL);
+    pthread_attr_destroy(&attr);
+    pthread_exit(NULL);
 
 }
 
